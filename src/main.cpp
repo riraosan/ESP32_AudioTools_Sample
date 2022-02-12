3b99e761a0eda710384379c00a9ca0faaf9dca32
riaosan <fiatpanda750@icloud.com>
2022-02-12 21:28:49 +0900

    Using SdFat

--- src/main.cpp
+++ src/main.cpp
@@ -1,55 +1,51 @@
+/**
+ * @file player-sd-a2dp.ino
+ * @brief see https://github.com/pschatzmann/arduino-audio-tools/blob/main/examples/examples-player/player-sd-a2dp/README.md
+ *
+ * @author Phil Schatzmann
+ * @copyright GPLv3
+ */
 
-// Add this in your sketch or change the setting in AudioConfig.h
-#define USE_A2DP
+// set this in AudioConfig.h or here after installing https://github.com/pschatzmann/arduino-libhelix.git
 #define USE_HELIX
+#define USE_A2DP
+#define SDFAT_FILE_TYPE 1
 
-#include <SPIFFS.h>
-#include "AudioTools.h"
-#include "AudioCodecs/CodecMP3Helix.h"
+#include <AudioTools.h>
+#include <AudioCodecs/CodecMP3Helix.h>
+#include <SPI.h>
+#include <SdFat.h>
 
 using namespace audio_tools;
 
-constexpr char SPEAKER_NAME[] = "Soundcore 3";
-constexpr char FILE_01[]      = "/non_BGM.mp3";
+SdFat SD;
+File  audioFile;
 
-A2DPStream _source = A2DPStream::instance();
-EncodedAudioStream _decoder(&_source, new MP3DecoderHelix());  // Decoding stream
-StreamCopy _copy;
-File _audioFile;
+A2DPStream         _source = A2DPStream::instance();  // A2DP input - A2DPStream is a singleton!
+MP3DecoderHelix    mp3decoder;
+EncodedAudioStream _decoder(&_source, &mp3decoder);  // Decoding stream
+StreamCopy         copier(_decoder, audioFile);
 
-// Arduino Setup
-void setup(void) {
-  //Serial.begin(115200);
-  //AudioLogger::instance().begin(Serial, AudioLogger::Info);
+// audioFile(mp3)->mp3decoder(helix)->_source(source);
+
+const int chipSelect = -1;
+
+void setup() {
+  // Serial.begin(115200);
+  // AudioLogger::instance().begin(Serial, AudioLogger::Warning);
+  SPI.begin(23, 33, 19, -1);
+
+  // setup file
+  SD.begin(SdSpiConfig(chipSelect, DEDICATED_SPI, SD_SCK_MHZ(10), &SPI));
+  audioFile = SD.open("/non.mp3");
 
   _source.setVolume(1.0);
-  _source.begin(TX_MODE, SPEAKER_NAME);
-  log_i("A2DP is connected now...");
-
-  if (!SPIFFS.begin()) {
-    log_e("SPIFFS Mount error.");
-    return;
-  } else {
-    log_i("SPIFFS Mounted...");
-  }
-
-  _audioFile = SPIFFS.open(FILE_01);
-  log_i("Open Audio File...");
+  _source.begin(TX_MODE, "Soundcore 3");
 
   _decoder.setNotifyAudioChange(_source);
   _decoder.begin();
-  log_i("Begin decoder...");
-
-  _copy.begin(_decoder, _audioFile);
-  log_i("Begin copy _audioFile to decoder...");
 }
 
 void loop() {
-  if (!_copy.copy()) {
-    auto info = _decoder.decoder().audioInfo();
-    log_i("The audio rate     from the mp3 file is %d", info.sample_rate);
-    log_i("The channels       from the mp3 file is %d", info.channels);
-    log_i("The bit per sample from the mp3 file is %d", info.bits_per_sample);
-    stop();
-  }
+  copier.copy();
 }
